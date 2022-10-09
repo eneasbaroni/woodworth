c21404e41d27bf6e88a28df75388111774d8d458
Eneas Baroni <eneasbaroni@gmail.com>
2022-10-09 20:19:55 -0300

    css error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import './grilla.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
